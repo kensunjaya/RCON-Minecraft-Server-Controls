297604fe7eeef018313f4c1ce4a7f137296bccea
Kenneth sunjaya <kennethsunjaya@gmail.com>
2024-12-25 21:16:43 +0700

    added latency check

--- src/app/components/playercard.tsx
+++ src/app/components/playercard.tsx
@@ -1,17 +1,18 @@
 import { Player } from "../interfaces/interface";
 import { sendCommand } from "../utilities/command";
+import Image from 'next/image';
 
-export const PlayerCard = ({ player, index }: { player: Player; index: number }) => {
+export const PlayerCard = ({ player }: { player: Player }) => {
   return (
     <div   
-      className="p-3 rounded-lg bg-no-repeat bg-cover text-white shadow-md my-3 text-lg font-mono flex justify-between items-center"
+      className="p-3 rounded-lg bg-no-repeat bg-cover text-white shadow-xl border my-3 text-lg font-mono flex justify-between items-center"
       style={{ 
         backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(/wood.jpg)', 
         backgroundSize: '100px', /* Or a value that fits your design */
         backgroundRepeat: 'repeat' /* Ensures the texture repeats to fill the area */
-      }}
-    >
-      <div className="font-mono text-xl">{index}. {player.name}</div>
+      }}>
+      <Image src={parseInt(player.name[0], 10) % 2 === 0 ? "/steve.jpg" : "/alex.jpg"} alt="" width={30} height={30} className="rounded-sm shadow-xl" />
+      <div className="font-mono text-xl ml-3">{player.name}</div>
       <button 
         className="minecraft-btn w-16 ml-auto mr-5 text-center truncate border-2 border-b-4 hover:text-yellow-200"
         onClick={() => {sendCommand(`/kick ${player.name}`)}}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { PlayerCard } from './components/playercard';
 import { Player } from './interfaces/interface';
 import { sendCommand } from './utilities/command';
+import { MdOutlineSignalCellularNodata } from "react-icons/md";
 
 export default function Home() {
   const [command, setCommand] = useState('');
@@ -11,6 +12,7 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const [serverIsOnline, setServerIsOnline] = useState(false);
   const [autoClearTextArea, setAutoClearTextArea] = useState(true);
+  const [latency, setLatency] = useState(0);
 
   const proceed = async () => {
     setLoading(true);
@@ -22,10 +24,21 @@ export default function Home() {
     setLoading(false);
   };
 
+  const checkLatency = async (url: string) => {
+    const startTime = performance.now();
+    try {
+      await fetch(url, { method: 'HEAD' });
+      const latency = performance.now() - startTime;
+      setLatency(latency);
+    } catch (error) {
+      console.error(error);
+      setLatency(-1);
+    }
+  }
+
   useEffect(() => {
     sendCommand('/list').then((response) => {
       setServerIsOnline(true);
-      console.log('Response:', response);
       const match = response.match(/There are \d+ of a max of \d+ players online:\s*(.*)/);
       if (match) {
         const playerList = match[1].split(', ');
@@ -33,15 +46,28 @@ export default function Home() {
           playerList[i] = { name: playerList[i] };
         }
         setPlayers(playerList);
-        console.log('Players currently online:', playerList);
-      } else {
-        console.log('No players online or failed to parse response');
       }
     }).catch(() => {
       setServerIsOnline(false);
     }).finally(() => {
       setLoading(false);
     });
+    
+    checkLatency('147.185.221.17');
+    setInterval(() => checkLatency('147.185.221.17'), 10000);
+    setInterval(() => sendCommand('/list').then((response) => {
+      setServerIsOnline(true);
+      const match = response.match(/There are \d+ of a max of \d+ players online:\s*(.*)/);
+      if (match) {
+        const playerList = match[1].split(', ');
+        for (let i = 0; i < playerList.length; i++) {
+          playerList[i] = { name: playerList[i] };
+        }
+        setPlayers(playerList);
+      }
+    }).catch(() => {
+      setServerIsOnline(false);
+    }), 20000);
   }, []);
 
   if (!loading && !serverIsOnline) {
@@ -54,24 +80,27 @@ export default function Home() {
 
   return (
     <main className="p-4 bg-center bg-cover bg-no-repeat h-screen text-white font-mono" style={{ backgroundImage: 'url(/background.webp)' }}>
-      <div>Online Players:</div>
+      <div className="flex justify-between text-lg">
+        <div>Online Players:</div>
+        {latency !== -1 ? <div>Ping: {latency} ms</div> : <MdOutlineSignalCellularNodata className="text-white text-lg"/>}
+      </div>
       <div>
-        {players.length > 0 ? (
+        {players.length > 0 && players[0].name !== "" ? (
           <ol>
             {players.map((player, index) => (
-              <PlayerCard key={index} index={index + 1} player={player}></PlayerCard>
+              <PlayerCard key={index} player={player}></PlayerCard>
             ))}
           </ol>
         ) : (
-          <p>No players online</p>
+          <div className="text-lg my-5">No players online</div>
         )}
       </div>
       <div className="flex flex-col gap-5">
         <textarea
           value={command}
           onChange={(e) => setCommand(e.target.value)}
-          placeholder="Enter RCON command"
-          className='border border-gray-800 rounded p-1 mr-2 text-black'
+          placeholder="Enter RCON command.."
+          className='border border-gray-800 rounded p-1 mr-2 my-3 text-black'
         />
         <button onClick={proceed} className='minecraft-btn mx-auto w-64 text-center truncate p-1 border-2 border-b-4 hover:text-yellow-200'>
           Send Command
